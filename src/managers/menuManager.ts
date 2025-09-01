d592d3ff8a10166ea64f90b07430b4d1cb778108
Anthony Manning-Franklin <4970703+Antman261@users.noreply.github.com>
2025-09-01 19:54:36 +0800

    Add standard menu elements (#1)
    * Added edit menu an standard items to enable copy pasting keyboard
    shortcuts
    * Added exit menu item to enable keyboard shortcut for exiting
    * Added keyboard shortcut to open sidebar (Cmd+,)
    * Added about section
    * Added MIT license

--- src/managers/menuManager.ts
+++ src/managers/menuManager.ts
@@ -1,4 +1,4 @@
-import { Menu, Submenu } from '@tauri-apps/api/menu';
+import { Menu, PredefinedMenuItem, Submenu } from '@tauri-apps/api/menu';
 import { sceneManager as sm } from './sceneManager';
 import { LokiMenuRoot } from './menuTypes';
 import {
@@ -40,7 +40,6 @@ const getRootMenu = (): LokiMenuRoot =>
           icon: saveAs,
           shortcut: 'Cmd+Shift+S',
         },
-        // Add an export image menu item
       ],
     },
   ] as const;
@@ -59,9 +58,42 @@ export const menuManager = {
   getRootMenu,
 };
 
-const toOsMenu = async (root: LokiMenuRoot) =>
-  Menu.new({
-    items: await Promise.all(
-      root.map((submenu) => Submenu.new({ ...submenu }))
-    ),
-  });
+const newEditWindow = async () => {
+  const items = await Promise.all([
+    PredefinedMenuItem.new({ item: 'Undo' }),
+    PredefinedMenuItem.new({ item: 'Redo' }),
+    PredefinedMenuItem.new({ item: 'Separator' }),
+    PredefinedMenuItem.new({ item: 'Cut' }),
+    PredefinedMenuItem.new({ item: 'Copy' }),
+    PredefinedMenuItem.new({ item: 'Paste' }),
+    PredefinedMenuItem.new({ item: 'Separator' }),
+    PredefinedMenuItem.new({ item: 'SelectAll' }),
+  ]);
+  return Submenu.new({ text: 'Edit', items });
+};
+
+const toOsMenu = async (root: LokiMenuRoot) => {
+  const items = await Promise.all(
+    root.map((submenu) => Submenu.new({ ...submenu })).concat(newEditWindow())
+  );
+
+  items[0].append(
+    await PredefinedMenuItem.new({
+      text: 'About Lokidraw',
+      item: {
+        About: {
+          name: 'Lokidraw',
+          credits:
+            'Made by Anthony Manning-Franklin (antman-codes.com), who stood atop the shoulders of Excalidraw.com',
+          copyright:
+            'MIT License\n\nCopyright (c) 2025 Anthony Manning-Franklin\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
+        },
+      },
+    })
+  );
+  items[0].append(await PredefinedMenuItem.new({ item: 'Services' }));
+  items[0].append(
+    await PredefinedMenuItem.new({ item: 'Quit', text: 'Quit Lokidraw' })
+  );
+  return Menu.new({ items });
+};
